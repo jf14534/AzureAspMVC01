77eea4c960e6784c90d284dcd4ec1af32a5b672b
user <user@I64>
2016-11-13 10:06:57 -0800

    Add project files.

---
+++ AzureAspMVC01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AzureAspMVC01.MvcApplication" Language="C#" %>
---
+++ AzureAspMVC01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureAspMVC01.Startup))]
+namespace AzureAspMVC01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
